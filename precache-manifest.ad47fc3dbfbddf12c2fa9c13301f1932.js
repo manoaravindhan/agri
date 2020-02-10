1b03350170865781d2beaa07a3d98665b7553ae4
manoaravindhan <man.oz@live.in>
2020-02-10 18:23:00 +0530

    Updates

---
+++ precache-manifest.ad47fc3dbfbddf12c2fa9c13301f1932.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b44780348de4f4f0a96c46515446b51",
+    "url": "/agrix/index.html"
+  },
+  {
+    "revision": "8d4887d3d9aae9aee0e0",
+    "url": "/agrix/static/css/2.4d9d6d08.chunk.css"
+  },
+  {
+    "revision": "51864acf46c8b20902e1",
+    "url": "/agrix/static/css/main.3d4ccffb.chunk.css"
+  },
+  {
+    "revision": "8d4887d3d9aae9aee0e0",
+    "url": "/agrix/static/js/2.05a2f35a.chunk.js"
+  },
+  {
+    "revision": "51864acf46c8b20902e1",
+    "url": "/agrix/static/js/main.33184274.chunk.js"
+  },
+  {
+    "revision": "9ea532a82b93f0e85fe1",
+    "url": "/agrix/static/js/runtime-main.684730e7.js"
+  },
+  {
+    "revision": "d2193e82872b98ab8889d7f83a3e3d1f",
+    "url": "/agrix/static/media/logo.d2193e82.png"
+  },
+  {
+    "revision": "523b36afd9ea5cad47c8436b57226069",
+    "url": "/agrix/static/media/result.523b36af.pdf"
+  }
+]);
